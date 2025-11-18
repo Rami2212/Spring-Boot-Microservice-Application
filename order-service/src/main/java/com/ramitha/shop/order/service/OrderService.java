db91e16bfce4639b4dea83db64d655ee1d38e882
Rami2212 <ramithapathmilarp@gmail.com>
2025-11-18 19:18:53 +0530

    add order tests

--- order-service/src/main/java/com/ramitha/shop/order/service/OrderService.java
+++ order-service/src/main/java/com/ramitha/shop/order/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.ramitha.shop.order.service;
 
+import com.ramitha.shop.order.client.InventoryClient;
 import com.ramitha.shop.order.dto.OrderRequest;
+import com.ramitha.shop.order.exception.OutOfStockException;
 import com.ramitha.shop.order.model.Order;
 import com.ramitha.shop.order.repository.OrderRepository;
 import lombok.AllArgsConstructor;
@@ -16,19 +18,26 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final InventoryClient inventoryClient;
+
     public Order placeOrder(OrderRequest orderRequest) {
 
-        Order order = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .skuCode(orderRequest.skuCode())
-                .price(orderRequest.price())
-                .quantity(orderRequest.quantity())
-                .build();
+        if (inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity())) {
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .skuCode(orderRequest.skuCode())
+                    .price(orderRequest.price())
+                    .quantity(orderRequest.quantity())
+                    .build();
 
-        orderRepository.save(order);
+            orderRepository.save(order);
 
-        log.info("Order placed successfully");
+            log.info("Order placed successfully");
 
-        return order;
+            return order;
+        } else {
+            log.error("Product is not in stock, cannot place order");
+            throw new OutOfStockException("Product is not in stock, cannot place order");
+        }
     }
 }
---
+++ order-service/src/test/java/com/ramitha/shop/order/repository/OrderRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.ramitha.shop.order.repository;
+
+import com.ramitha.shop.order.model.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class OrderRepositoryTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    void saveOrder_shouldSaveOrderSuccessfully() {
+        Order order = Order.builder()
+                .orderNumber("ORD123456")
+                .skuCode("SKU123")
+                .price(BigDecimal.valueOf(50))
+                .quantity(1)
+                .build();
+
+        orderRepository.save(order);
+
+        Order savedOrder = orderRepository.findById(order.getId()).orElse(null);
+        assert savedOrder != null;
+        assert savedOrder.getOrderNumber().equals("ORD123456");
+        assert savedOrder.getSkuCode().equals("SKU123");
+        assert savedOrder.getPrice().equals(BigDecimal.valueOf(50));
+        assert savedOrder.getQuantity() == 1;
+
+        orderRepository.delete(order);
+    }
+}

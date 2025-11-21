423b9cc037b4eabe6134cf7a0e1545a9916d1208
Rami2212 <ramithapathmilarp@gmail.com>
2025-11-21 20:44:47 +0530

    add rest client

---
+++ order-service/src/main/java/com/ramitha/shop/order/config/RestClientConfig.java
@@ -0,0 +1,22 @@
+package com.ramitha.shop.order.config;
+
+import com.ramitha.shop.order.client.InventoryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl("http://localhost:8082")
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+}

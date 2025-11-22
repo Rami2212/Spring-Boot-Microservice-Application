c95593f7db21d560bb515cc10d737dc1b5c8beb4
Rami2212 <ramithapathmilarp@gmail.com>
2025-11-22 16:37:19 +0530

    add frontend

--- gateway-service/src/main/java/com/ramitha/shop/gateway/routes/Routes.java
+++ gateway-service/src/main/java/com/ramitha/shop/gateway/routes/Routes.java
@@ -21,7 +21,7 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
         return route("product-service")
-                .route(RequestPredicates.path("/api/products"), HandlerFunctions.http("http://localhost:8080"))
+                .route(RequestPredicates.path("/api/products/**"), HandlerFunctions.http("http://localhost:8080"))
                 .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
                         URI.create("forward:/fallbackRoute")))
                 .build();

e9ec779dcda60efed7a6d24c5d23766988f4b161
Rami2212 <ramithapathmilarp@gmail.com>
2025-12-04 11:46:45 +0530

    add observability

--- gateway-service/src/main/java/com/ramitha/shop/gateway/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/ramitha/shop/gateway/config/SecurityConfig.java
@@ -20,7 +20,8 @@ public class SecurityConfig {
             "/swagger-ui.html",
             "/swagger-resources/**",
             "/api-docs/**",
-            "/aggregate/**"
+            "/aggregate/**",
+            "/actuator/prometheus"
     };
 
     @Bean
